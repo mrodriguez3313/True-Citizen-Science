6951e88328a0d02ce3aa150382ba39a08dd08af0
Marco Rodriguez <marco.rodriguez113@gmail.com>
2020-02-03 16:49:17 -0800

    current main.go works, but as design

--- src/main.go
+++ src/main.go
@@ -5,23 +5,21 @@ import (
 	"fmt"
 	"log"
 	"strings"
-	"encoding/json"
-	"time"
+	// "encoding/json"
 	"context"
 	"regexp"
-	"reflect"
-	// "io"
-	// "io/ioutil"
+	// "reflect"
 	"os"
 	// "os/exec"
 
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/bson"
+	// "go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type IPFSContent struct {
-	Filename string
+	Projectname string
+	Filecontent string
 }
 
 
@@ -30,74 +28,79 @@ func main() {
 	// initializing client connection to mongodb service
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil { log.Fatal(err) }
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-	defer cancel()
+
 
 	// Connect to MongoDB
-	err = client.Connect(ctx)
+	err = client.Connect(context.Background())
 	if err != nil { log.Fatal(err) }
 	fmt.Println("connected to MongoDB!")
 
-	// This is correct. Lines is an array of strings where the index has the content of each file.
+
+	db := client.Database("PrivateIPFSDB")
+	collection := db.Collection("AllProjects")
+	var insertRes *mongo.InsertOneResult
+	findOptions := options.Find()
+	findOptions.SetLimit(5)
+	// ipfscontent := []interface{}
+
+	// lines is an array of strings where the index has the content of each file.
 	lines, err := scanLines("/home/marco/Documents/InsightDC/hash_outputs")
 	if err != nil { log.Fatal(err) }
 	tokenizer := regexp.MustCompile(`;`)
 
-	// ipfscontent := make(map[string]interface{})
-
-	var collection *mongo.Collection
-	// For every line in file: tokenize the line, remove the tag, create object, insert to mongodb in the 'tag' collection
+	// For every line in file: tokenize the line, remove the tag, create object, insert to mongodb
 	for idx, _ := range lines {
-		// each iteration tag is being reinitialized [tag, string w/o tag]
+		// 51: This line will split file input between projectname and filecontent
 		split_input := tokenizer.Split(string(lines[idx]), 2)
+		// 53: assigns projectname to tag
 		tag := split_input[0]
-		// fmt.Println("tag: ", tag)
+		// 54: initializes object to insert into db.
+		ipfsFile := &IPFSContent{
+			Projectname: tag,
+			Filecontent: strings.TrimLeft(split_input[1], " "),
+		}
 
-		// Creating database called PrivateIPFSDB. with table called Files
-		collection := client.Database("PrivateIPFSDB").Collection(tag)
-		// fmt.Println("Collection type:", reflect.TypeOf(collection), "\n")
-		jsonIpfsContent, err := json.Marshal(strings.TrimLeft(split_input[1], " "))
-		if err != nil { log.Fatal(err) }
-
-		// fmt.Println("jsonIpfsContent: ", string(jsonIpfsContent))
-		ipfsFile := &IPFSContent{ string(jsonIpfsContent) }
-		// fmt.Println("ifpsfile: ", ipfsFile)
-		insertRes, err := collection.InsertOne(context.TODO(), ipfsFile)
+		insertRes, err = collection.InsertOne(context.TODO(), ipfsFile)
 		if err != nil { log.Fatal(err) }
 
 		fmt.Println("inserted one file: ", insertRes.InsertedID)
 	}
 
-	findOptions := options.Find()
-	collection = client.Database("PrivateIPFSDB").Collection("Beaver")
-	cursor, err := collection.Find(context.TODO(), bson.D{{}}, findOptions)
-	if err != nil {
-		fmt.Println("Finding all documents ERROR:", err)
-		log.Fatal(err)
-		defer cursor.Close(ctx)
-	}
-		// iterate over docs using Next()
-	for cursor.Next(ctx) {
-		// Declare a result BSON object
-		var result bson.M
-		err := cursor.Decode(&result)
-		if err != nil {
-			fmt.Println("cursor.Next() error:", err)
-			log.Fatal(err)
-		}
-		fmt.Println("\nresult type:", reflect.TypeOf(result))
-		fmt.Println("result:", result)
+	fmt.Println("now were here.")
+	// collection = client.Database("PrivateIPFSDB").Collection("Beaver")
+	// cur, err := *collection.Find(context.Background(), bson.D{})
+	// check := FindRecords(collection)
+	// fmt.Println("a element: ", check[0])
+	// if err != nil {
+	// 	fmt.Println("Finding all documents ERROR:", err)
+	// 	log.Fatal(err)
+	// }
+	// defer cur.Close(context.Background())
+	// fmt.Println("passed error check of cursor.")
+	// 	// iterate over docs using Next()
+	// for cur.Next(context.TODO()) {
+	// 	fmt.Println("inside for loop,")
+	// 	// Declare a result BSON object
+	// 	var result bson.M
+	// 	err := cur.Decode(&result)
+	// 	if err != nil {
+	// 		fmt.Println("cursor.Next() error:", err)
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Println("\nresult type:", reflect.TypeOf(result))
+	// 	fmt.Println("result:", result)
+	//
+	// }
+
+	// err = client.Disconnect(context.TODO())
+	// if err != nil { log.Fatal(err)}
+	//
+	// fmt.Println("Connection to MongoDB closed.")
 
 	}
 
-	err = client.Disconnect(context.TODO())
-	if err != nil { log.Fatal(err)}
-
-	fmt.Println("Connection to MongoDB closed.")
-
-	}
-
-	func scanLines(path string) ([]string, error) {
+	// This function takes in a file path and returns a string where each index in the array is every line in the file
+func scanLines(path string) ([]string, error) {
 
   file, err := os.Open(path)
   if err != nil {
@@ -118,3 +121,79 @@ func main() {
 
   return lines, nil
 }
+
+
+
+//Find multiple documents
+// func FindRecords(db mongo.Collection) ([]IPFSContent){
+//     // err := godotenv.Load()
+// 		//
+//     // if err != nil {
+//     //     fmt.Println(err)
+//     // }
+// 		//
+//     // //Get database settings from env file
+//     // //dbUser := os.Getenv("db_username")
+//     // //dbPass := os.Getenv("db_pass")
+//     // dbName := os.Getenv("db_name")
+//     // docCollection := "retailMembers"
+// 		//
+//     // dbHost := os.Getenv("db_host")
+//     // dbPort := os.Getenv("db_port")
+//     // dbEngine := os.Getenv("db_type")
+// 		//
+//     // //set client options
+//     // clientOptions := options.Client().ApplyURI("mongodb://" + dbHost + ":" + dbPort)
+//     // //connect to MongoDB
+//     // client, err := mongo.Connect(context.TODO(), clientOptions)
+//     // if err != nil {
+//     //     log.Fatal(err)
+//     // }
+// 		//
+//     // //check the connection
+//     // err = client.Ping(context.TODO(), nil)
+//     // if err != nil {
+//     //     log.Fatal(err)
+//     // }
+// 		//
+//     // fmt.Println("Connected to " + dbEngine)
+//     // db := client.Database(dbName).Collection(docCollection)
+//
+//     //find records
+//     //pass these options to the Find method
+//     //Set the limit of the number of record to find
+//
+//     //Define an array in which you can store the decoded documents
+//     var results []IPFSContent
+//
+//     //Passing the bson.D{{}} as the filter matches  documents in the collection
+//     cur, err := db.Find(context.TODO(), bson.D{{}}, findOptions)
+//     if err !=nil {
+//         log.Fatal(err)
+//     }
+//
+//     //Finding multiple documents returns a cursor
+//     //Iterate through the cursor allows us to decode documents one at a time
+//     for cur.Next(context.TODO()) {
+//         //Create a value into which the single document can be decoded
+//         var elem IPFSContent
+//         err := cur.Decode(&elem)
+//         if err != nil {
+//             log.Fatal(err)
+//         }
+//
+//         results=append(results, elem)
+//
+//     }
+//
+//     if err := cur.Err(); err != nil {
+//         log.Fatal(err)
+//     }
+//
+//     //Close the cursor once finished
+//     cur.Close(context.TODO())
+//
+//     fmt.Printf("Found multiple documents: %+v\n", results)
+//
+// 		return results
+// }
--- src_scripts/README.md
+++ src_scripts/README.md
@@ -5,14 +5,14 @@ kill_scripts.sh
 script.sh
 demo.sh
 
-autoUpload.sh
+autoUpload.sh 
 	This file will upload any file that is in the previous directory called *filesdump*. If it is not there then it will not throw an error, it just won't do anything. This file requires a package be installed called 'inotifywait' that is the core of this automation process that says: if a file is created in said direcory, run this command.
-	In our case we are running 'ipfs add'. Which implies that you must have ipfs daemon isntalled and running aswell to run this script.
+	In our case we are running 'ipfs add'. Which implies that you must have ipfs daemon isntalled and running aswell to run this script. 
 
 file_generator.py
-	This is a python script used to simulate logs/files being contributed by multiple sources and being automatically stored in 'filesdump'. If the directory does not exist, an error will be thrown. Currently this is a single threaded process, but you can create more threads and by changing the variable 'num_threads' you can also change how many files get created by changing the variable 'files_to_create'
+	This is a python script used to simulate logs/files being contributed by multiple sources and being automatically stored in 'filesdump'. If the directory does not exist, an error will be thrown. Currently this is a single threaded process, but you can create more threads and by changing the variable 'num_threads' you can also change how many files get created by changing the variable 'files_to_create' 
 
-kill_scripts.sh
+kill_scripts.sh 
 	This script will kill the file_generator.py and autoUpload.sh. As they are meant to be run in the background. To run this script you need to have the package 'pgrep' installed.
 
 script.sh
