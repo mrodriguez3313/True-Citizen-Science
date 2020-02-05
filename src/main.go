bf076f1615b12732e238ee49d16c993c1b6e0d7d
Marco Rodriguez <marco.rodriguez113@gmail.com>
2020-02-04 17:28:12 -0800

    all programs as intended, need to udpate Readmes

--- src/main.go
+++ src/main.go
@@ -21,11 +21,11 @@ type IPFSContent struct {
 }
 
 
-
 func main() {
 
+	// We check this again later on, but this is here to short circuit that check. In case something was provided but it was not a file.
 	if len(os.Args) == 1 {
-		fmt.Println("Please provide 1 or more files to add to mongodb.")
+		fmt.Println("Please provide 1 or more files to add to mongodb. This file may be a file with many elements or a single element.")
 		os.Exit(0)
 	}
 	// initializing client connection to mongodb service
@@ -41,19 +41,23 @@ func main() {
 	// creating Database and collection for easily importing to it later on.
 	db := client.Database("PrivateIPFSDB")
 	collection := db.Collection("AllProjects")
-	// var insertRes *mongo.InsertOneResult
 
-
+	// Input checking if args provided are valid files. Else the program exits.
 	Arg := os.Args[1:]
 	if len(Arg) >= 1 {
-		for _, file := range Arg {
+		for idx, file := range Arg {
 			if _, err := os.Stat(file); err == nil {
 				// file exists, add it into the db
-				autoscan(file, db, collection, client)
-				return
+				import_file(file, db, collection, client)
 			} else if os.IsNotExist(err) {
-				// file doesn't exist
-				log.Fatal(err)
+					// file doesn't exist
+					if idx == len(Arg)-1 {
+						fmt.Printf("File %s does not exist.\n", file)
+						os.Exit(1)
+					} else {
+					fmt.Printf("Sorry file %s does not exist. Trying next file...\n", file)
+					continue
+				}
 			} else {
 				// mmmmm something bad happened see error for details
 				log.Fatal(err)
@@ -63,34 +67,15 @@ func main() {
 		fmt.Println("Please provide 1 or more files to add to mongodb.")
 		os.Exit(0)
 	}
-
+	os.Exit(0)
 }
 
-// This function takes in a file path and returns a string where each index in the array is every line in the file
-func scanLines(path string) ([]string, error) {
 
-  file, err := os.Open(path)
-  if err != nil {
-     return nil, err
-  }
-
-  defer file.Close()
-
-  scanner := bufio.NewScanner(file)
-
-  scanner.Split(bufio.ScanLines)
-
-  var lines []string
-
-  for scanner.Scan() {
-    lines = append(lines, scanner.Text())
-  }
-
-  return lines, nil
-}
-
-func autoscan(file string, db *mongo.Database, collection *mongo.Collection, client *mongo.Client) {
-	// lines is an array of strings where the index has the content of each file.
+// This function will insert a file into the database.
+// INPUT: a file or path to file to parse each line and insert each line into db.
+// Closes connection after done inserting every line in file.
+func import_file(file string, db *mongo.Database, collection *mongo.Collection, client *mongo.Client) {
+	// 71: lines is will hold an array of strings where the index has the content of each file.
 	lines, err := scanLines(file)
 	if err != nil { log.Fatal(err) }
 	tokenizer := regexp.MustCompile(`;`)
@@ -115,7 +100,37 @@ func autoscan(file string, db *mongo.Database, collection *mongo.Collection, cli
 	}
 
 	err = client.Disconnect( context.TODO() )
-	if err != nil { log.Fatal(err)}
+	if err != nil { log.Fatal(err) }
 
 	fmt.Println("Connection to MongoDB closed.")
+}
+
+
+// This function takes in a file path and returns a list of strings where each index in the array is every line in the file
+func scanLines(path string) ([]string, error) {
+	// Does not do any formatting checking. File provided MUST be a new file on each line. Each line must be in format: '<Projectname>; <Filecontent>'
+	// A solution is to have the data, (BEFORE ADDING TO IPFS), in json format.
+	//  This could be achieved by providing the user/citizen a simple n-steps, step-by-step process to add the data into a json file; where n is determined by the researcher for how many samples/data points they want to collect.
+	file, err := os.Open(path)
+  if err != nil {
+     return nil, err
+  }
+
+  defer file.Close()
+	// Create scanner object to look through file
+  scanner := bufio.NewScanner(file)
+	// scanner will now only look line by line in the file
+  scanner.Split(bufio.ScanLines)
+
+  var lines []string
+	// iterate through each line in file
+  for scanner.Scan() {
+		// pull out the text
+		text := scanner.Text()
+		// creating list of every line in file
+    lines = append(lines, text)
+		// fmt.Println("current line: ", text)
+  }
+
+  return lines, nil
 }
--- src_scripts/file_generator.py
+++ src_scripts/file_generator.py
@@ -1,12 +1,15 @@
 import threading
 import time
+import random
+
+file_categories = ["Beaver", "Mice", "Beach", "Mountains"]
 
 def create_files():
     # each thread will send a new output to a new file
-    file_num = 0
-    while True:
+    files_to_create = 10
+    for idx in range(files_to_create):
         with open("/home/marco/Documents/InsightDC/filesdump/hello_world{}.txt".format(int(round(time.time() * 10000))), "w") as f:
-            f.write((''.join('{}, {}'.format(threading.current_thread().ident, file_num))))
+            f.write((''.join('{}; {}, {}'.format(random.choice(file_categories), threading.current_thread().ident, idx))))
         # print("file written by thread {}.".format(threading.current_thread().ident))
         time.sleep(5)
     return
