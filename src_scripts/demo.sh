f2b3715a198aa23c15b27edf3acebdd0216ca590
Marco Rodriguez <marco.rodriguez113@gmail.com>
2020-02-12 18:16:42 -0800

    final demo

--- src_scripts/demo.sh
+++ src_scripts/demo.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-docker stop ipfsnode
-docker start ipfsnode
-./autoUpload.sh &
+# docker stop ipfsnode
+# docker start ipfsnode
+# rm -r ../filesdump; rm ../Hashes; rm ../hash_outputs
 ./script.sh &
+sleep 2
+./autoUpload.sh &
 
-sleep(20)
+
+sleep 10
 ./kill_scripts.sh
+cd ..
+pwd
+sleep 4
+echo ""
+echo "Number of files created: "
+ls -l filesdump | wc -l
+echo ""
+sleep 2
+echo "extracting hashes: "
+sleep 1
+./extract_hashes_batch Hashes
+echo ""
+sleep 6
+rm -r filesdump; rm Hashes; rm hash_outputs
+mongo
--- src_scripts/file_generator.py
+++ src_scripts/file_generator.py
@@ -1,28 +1,44 @@
 import threading
 import time
 import random
+import os
+from signal import *
+import sys
 
 file_categories = ["Beaver", "Mice", "Beach", "Mountains"]
 
 def create_files():
     # each thread will send a new output to a new file
-    files_to_create = 10
+    files_to_create = 20
     for idx in range(files_to_create):
         with open("/home/marco/Documents/InsightDC/filesdump/hello_world{}.txt".format(int(round(time.time() * 10000))), "w") as f:
             f.write((''.join('{}; {}, {}'.format(random.choice(file_categories), threading.current_thread().ident, idx))))
-        # print("file written by thread {}.".format(threading.current_thread().ident))
-        time.sleep(5)
+        time.sleep(1)
     return
 
-def main():
-    threads = list()
-    for num_threads in range(1):
-        x = threading.Thread(target=create_files)
-        threads.append(x)
-        x.start()
-
+def join_threads(threads):
     for index, thread in enumerate(threads):
         thread.join()
 
+def main():
+    threads = list()
+    path="/home/marco/Documents/InsightDC/filesdump/"
+    isdir = os.path.isdir(path)
+    if not isdir:
+        try:
+            os.mkdir(path)
+        except OSError:
+            print("Directory at %s failed to be created." % path)
+            return
+        else:
+            print("Directory at %s created." % path)
 
+    for num_threads in range(1):
+        x = threading.Thread(target=create_files)
+        threads.append(x)
+        time.sleep(1)
+        x.start()
+
+
+    join_threads(threads)
 main()
